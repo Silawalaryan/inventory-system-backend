52ae9a24fa6e3222a346f2a2dd15ab4ef1f660de
Aryan Silawal <silawalaryan16@gmail.com>
2025-07-22 21:00:18 +0545

    Minor changes to room routes.

--- src/routes/room.routes.js
+++ src/routes/room.routes.js
@@ -12,11 +12,13 @@ import { verifyAdmin } from "../middlewares/admin_check.middleware.js";
 
 const router = Router();
 
-router.route("/").post(verifyJwt,verifyAdmin,addNewRoom);
-router.route("/").get(verifyJwt,displayAllRooms);
-router.route("/:id").patch(verifyJwt,verifyAdmin,updateRoomDetails);
-router.route("/:id").delete(verifyJwt,verifyAdmin,deleteRoom);
-router.route("/floor-filter/:floor_id/:page").get(verifyJwt,filterRoomsByFloor)
-router.route("/search/:room_string/:page").get(verifyJwt,getRoomSearchResults)
+router.route("/").post(verifyJwt, verifyAdmin, addNewRoom);
+router.route("/:page").get(verifyJwt, displayAllRooms);
+router.route("/:id").patch(verifyJwt, verifyAdmin, updateRoomDetails);
+router.route("/:id").delete(verifyJwt, verifyAdmin, deleteRoom);
+router
+  .route("/floor-filter/:floor_id/:page")
+  .get(verifyJwt, filterRoomsByFloor);
+router.route("/search/:room_string/:page").get(verifyJwt, getRoomSearchResults);
 
 export default router;
